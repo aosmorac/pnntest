3b888e6f568070f1f194801e9188615559fa080e
Abel Moreno <moreno.abel@gmail.com>
2017-03-15 17:35:18 -0500

    Ajax con info de formulario

--- module/Application/src/Application/Controller/PruebaController.php
+++ module/Application/src/Application/Controller/PruebaController.php
@@ -1,6 +1,6 @@
 <?php
 /* * ********************************************************
- * CLIENTE: UMAIC
+ * CLIENTE: PNN
  * ========================================================
  *
  * @copyright Abel Oswaldo Moreno Acevedo 2016
@@ -18,88 +18,13 @@ use Zend\Debug\Debug;
 use Application\Model\Data;
 
 class PruebaController extends AbstractActionController
-{
-    public function dashboardAction()
+{    
+    public function updateFormAction()
     {
-        return new ViewModel();
-    }
-    
-    public function dataAction()
-    {
-        return new ViewModel();
-    }
-    
-    public function updateFileFormAction()
-    {
-        return new ViewModel();
-    }
-    
-    public function getDataFileAction()
-    {
-        $request = $this->getRequest(); 
-        $file = $request->getFiles()->toArray();
-        $data = new Data(); 
-        $data->saveFile($file['mcFile']['tmp_name'], 'ARCHIVO');
-        return new ViewModel();
-    }
-    
-    public function exportAllAction(){
-        $filename = 'UMAIC-Prueba.csv';
-        $info = new Data();
-        $resultset = $info->getInformacionCargada();
-        
-        $columnHeaders = Array(
-                                      'ID'
-                                    , 'Proyecto Codigo'
-                                    , 'Proyecto Nombre'
-                                    , 'Proyecto Descripcion'
-                                    , 'Proyecto Inicio'
-                                    , 'Proyecto Fin'
-                                    , 'Proyecto Estado'
-                                    , 'Organizacion Sigla'
-                                    , 'Organizacion Nombre'
-                                    , 'Organizacion Tipo'
-                                    , 'Presupuesto Monto'
-                                    , 'Presupuesto Tema'
-                                    , 'Beneficiarios Total'
-                                    , 'Beneficiarios Hombre'
-                                    , 'Beneficiarios Mujeres'
-                                    , 'Departamento'
-                                    , 'Decha de Carga'
-                                    , 'Fuente de carga'
-        );
-        
-        $view = new ViewModel();
-        $view->setTemplate('download/download-csv')
-             ->setVariable('results', $resultset)
-             ->setTerminal(true);
-
-        if (!empty($columnHeaders)) {
-            $view->setVariable(
-
-                'columnHeaders', $columnHeaders
-            );
-        }
-
-        $output = $this->getServiceLocator()
-                       ->get('viewrenderer')
-                       ->render($view);
-
-        $response = $this->getResponse();
-
-        $headers = $response->getHeaders();
-        $headers->addHeaderLine('Content-Type', 'text/csv')
-                ->addHeaderLine(
-
-                    'Content-Disposition', 
-                    sprintf("attachment; filename=\"%s\"", $filename)
-                )
-                ->addHeaderLine('Accept-Ranges', 'bytes')
-                ->addHeaderLine('Content-Length', strlen($output));
-
-        $response->setContent($output);
-
-        return $response;
+    	$request = $this->getRequest();
+    	$data = 'Fecha: '.$request['fecha'].' -- Texto: '.$request['texto'].' -- Departamento: '.$request['departamento'].' -- Municipio: '.$request['municipio'].'';
+        echo $data;
+    	return new ViewModel();
     }
     
 }
